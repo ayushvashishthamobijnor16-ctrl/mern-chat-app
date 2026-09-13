72f16e92b2c80aaea7091131585ea6325b76edbc
ayushvashishthamobijnor16-ctrl <ayushvashishtha.mo.bijnor16@gmail.com>
2026-09-13 14:58:34 +0530

    Add backend URL for production

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:5001/api"
-      : "https://mern-chat-backend.onrender.com/api",
+      : " https://mern-chat-app-0oml.onrender.com",
   withCredentials: true,
 });
