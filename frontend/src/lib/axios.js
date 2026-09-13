851f56a5452929cf064d04031bb3a165c7b143ef
ayushvashishthamobijnor16-ctrl <ayushvashishtha.mo.bijnor16@gmail.com>
2026-09-13 16:29:22 +0530

    Fix Render deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -4,6 +4,6 @@ export const axiosInstance = axios.create({
   baseURL:
     import.meta.env.MODE === "development"
       ? "http://localhost:5001/api"
-      : " https://mern-chat-app-0oml.onrender.com",
+      : "https://mern-chat-app-0oml.onrender.com/api",
   withCredentials: true,
 });
